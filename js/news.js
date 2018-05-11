3da44bae8ccf0e4fb365734e9ed2d48c87e8fe87
alishaibz <32881254+alishaibz@users.noreply.github.com>
2018-05-11 03:17:23 +0200

    Add files via upload

--- js/news.js
+++ js/news.js
@@ -3,29 +3,19 @@ var cargado = 1;
 
 
 //****************************************************************************************************
-//AL CARGAR LA PÁGINA
 
-$(document).ready(function(){
-        $('#boton_mas').text('No hay');
-	$(window).scroll(function () {
-		$('#boton_mas').text('scroll');
-    		if ($(window).scrollTop() + $(window).height()>= $(document).height()) {
-		$('#boton_mas').text('entra');
-        		if (cargado <=2) {
-            			$.getJSON("https://rawgit.com/alishaibz/LMSGI06/master/json/data" + cargado + ".json", function (jsonObject) {
-                		addrow(jsonObject);
-            		}); cargado++;
-        		} else {
-            			$('#boton_mas').text('No hay más noticias');
-        		}
-    		}
-	});
-
+$(window).scroll(function () {
+	if ($(window).scrollTop() + $(window).height()>= $(document).height()) {
+		if (cargado <=2) {
+            		$.getJSON("https://rawgit.com/alishaibz/LMSGI06/master/json/data" + cargado + ".json", function (jsonObject) {
+                	addrow(jsonObject);
+            	}); cargado++;
+        	} else {
+            		$('#boton_mas').text('No hay más noticias');
+        	}
+    	}
 });
 
-
-
-//****************************************************************************************************
 
 function cargar() {
     if (cargado <= 2) {
@@ -37,9 +27,9 @@ function cargar() {
     }
 };
 
+
 function addrow(json) {
     $(".notis").append("<div class='row noticias_" + cargado + "'>");
-
     $.each(json, function (i, item) {
         $(".noticias_" + cargado).append('<div class="col-lg-4">' + '<a href="news.html">' + '<div class="pic_div">' + '<img src="' + item.imgmid + '" class="news_pic" alt="Image">' + '</div>' + '<div class="caption">' + '<h4 class="text-justify">' + item.title + '</h4>' + '<p class="text-justify">' + item.description + '</p>' +
 	'<p class="text-right">' + '<em>' + item.datetime + '</em>' + '</p>' + '</div>' + '</a>' + '</div>');
